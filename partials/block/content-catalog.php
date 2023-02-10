ebcb9b17c41e10f405fa80c851b376dc2ef8496b
kundius <kundius.ruslan@gmail.com>
2023-02-10 16:03:23 +0300

    upd

--- partials/block/content-catalog.php
+++ partials/block/content-catalog.php
@@ -1,27 +1,43 @@
 <?php if ($catalog = get_field('catalog')): ?>
-  <div class="catalog-list">
-    <?php foreach ($catalog as $item): ?>
-      <div class="catalog-list__cell">
-        <div class="catalog-item">
-          <?php if ($item['image']): ?>
-            <div class="catalog-item__image">
-              <img src="<?php echo $item['image']['sizes']['theme-medium'] ?>" />
-            </div>
-          <?php endif; ?>
-          <div class="catalog-item__body">
-            <?php if ($item['name']): ?>
-              <div class="catalog-item__name">
-                <?php echo $item['name'] ?>
-              </div>
-            <?php endif; ?>
-            <?php if ($item['desc']): ?>
-              <div class="catalog-item__desc">
-                <?php echo $item['desc'] ?>
-              </div>
-            <?php endif; ?>
+<div class="frame-house">
+  <?php foreach ($catalog as $item): ?>
+  <div class="frame-house-row">
+    <div class="frame-house-layout">
+      <?php if ($item['image']): ?>
+        <div class="frame-house-layout__left">
+          <div class="frame-house-image">
+            <img src="<?php echo $item['image']['sizes']['theme-medium'] ?>" />
           </div>
         </div>
+      <?php endif; ?>
+
+      <div class="frame-house-layout__right">
+        <div class="frame-house-details">
+          <div class="frame-house-details__title">
+            <?php echo $item['name'] ?>
+          </div>
+          <div class="frame-house-details__desc">
+            <?php echo $item['desc'] ?>
+          </div>
+        </div>
+
+        <?php if ($files = get_field('files')): ?>
+        <div class="frame-house-files">
+          <?php foreach ($files as $file): ?>
+          <div class="frame-house-file">
+            <div class="frame-house-file__icon">
+              <img src="<?php echo $file['icon']['url'] ?>" />
+            </div>
+            <a href="<?php echo $file['file']['url'] ?>" class="frame-house-file__name" target="_blank">
+              <?php echo $file['name'] ?>
+            </a>
+          </div>
+          <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
       </div>
-    <?php endforeach; ?>
+    </div>
   </div>
+  <?php endforeach; ?>
+</div>
 <?php endif; ?>
