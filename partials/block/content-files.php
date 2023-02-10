541e079c9c363c111faf088fe3bc0580fd2274d9
kundius <kundius.ruslan@gmail.com>
2023-02-10 15:28:47 +0300

    upd

--- partials/block/content-files.php
+++ partials/block/content-files.php
@@ -1,30 +1,18 @@
-<?php
-/**
- * Block Name: Testimonial
- *
- * This is the template that displays the testimonial block.
- */
-
-// get image field (array)
-$avatar = get_field('avatar');
-
-// create id attribute for specific styling
-$id = 'testimonial-' . $block['id'];
-
-// create align class ("alignwide") from block setting ("wide")
-$align_class = $block['align'] ? 'align' . $block['align'] : '';
-
-?>
-<blockquote id="<?php echo $id; ?>" class="testimonial <?php echo $align_class; ?>">
-    <p><?php the_field('testimonial'); ?></p>
-    <cite>
-        <img src="<?php echo $avatar['url']; ?>" alt="<?php echo $avatar['alt']; ?>" />
-        <span><?php the_field('author'); ?></span>
-    </cite>
-</blockquote>
-<style type="text/css">
-    #<?php echo $id; ?> {
-        background: <?php the_field('background_color'); ?>;
-        color: <?php the_field('text_color'); ?>;
-    }
-</style>
+<?php if ($files = get_field('files')): ?>
+  <div class="catallog-file-list">
+    <div class="file-list">
+      <?php foreach ($filelist as $item): ?>
+        <div class="file-item">
+          <?php if ($item['icon']): ?>
+            <div class="file-item__icon">
+              <img src="<?php echo $item['icon']['url'] ?>" />
+            </div>
+          <?php endif; ?>
+          <a href="<?php echo $item['file']['url'] ?>" class="file-item__name" target="_blank">
+            <?php echo $item['name'] ?>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+<?php endif; ?>
