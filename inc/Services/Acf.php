d6834e925b0c4f79087067f0c3cae694a77ab15a
kundius <kundius.ruslan@gmail.com>
2023-02-10 15:13:20 +0300

    upd

--- inc/Services/Acf.php
+++ inc/Services/Acf.php
@@ -32,6 +32,32 @@ class Acf implements Service
             'capability' => 'edit_posts',
             'redirect' => false,
         ));
+
+        add_action('acf/init', function() {
+          // check function exists
+          if (function_exists('acf_register_block')) {
+            // register a testimonial block
+            acf_register_block(array(
+              'name'              => 'files',
+              'title'             => __('Files'),
+              'description'       => __('A custom files block.'),
+              'render_callback'   => [$this, 'acf_block_render_callback'],
+              'category'          => 'formatting',
+              'icon'              => 'admin-comments',
+              'keywords'          => array( 'files', 'quote' ),
+            ));
+          }
+        });
+    }
+
+    function acf_block_render_callback($block) {
+      // convert name ("acf/testimonial") into path friendly slug ("testimonial")
+      $slug = str_replace('acf/', '', $block['name']);
+
+      // include a template part from within the "template-parts/block" folder
+      if (file_exists( get_theme_file_path("/partials/block/content-{$slug}.php"))) {
+        include( get_theme_file_path("/partials/block/content-{$slug}.php") );
+      }
     }
 
     /**
